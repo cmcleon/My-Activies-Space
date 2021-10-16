33233083ef09af41925dc60a5c605d8c8d2595ac
cmcleon <candicem.leon@gmail.com>
2021-10-16 18:01:25 -0300

    :up: autenticacao user

--- SENAI/Semana2/mas-backend/src/routes.ts
+++ SENAI/Semana2/mas-backend/src/routes.ts
@@ -2,15 +2,18 @@ import {Router, Request, Response, response}  from 'express';
 import { UserController } from './controller/UserController';
 import {ActivyController} from './controller/ActivyController';
 import {CourseUnitController} from './controller/CourseUnitController';
+import {AuthenticateController} from './controller/AuthenticateController';
 
 const userController = new UserController();
 const activyController = new ActivyController();
 const courseUnitController = new CourseUnitController();
+const authenticateController = new AuthenticateController();
 
 
 const routes = Router();
 
 routes.post('/user', userController.create);
+routes.post('/auth', authenticateController.create);
 routes.post('/activy', activyController.create);
 routes.post('/courseuunit', courseUnitController.create);
 
